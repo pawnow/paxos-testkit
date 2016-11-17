278791be038b4c30db673bc1fe1baae9a8b36c38
domin4815 <domin4815@gmail.com>
2016-11-17 23:35:17 +0100

    Kill acceptor

--- src/test/groovy/integration/Presentation.groovy
+++ src/test/groovy/integration/Presentation.groovy
@@ -140,4 +140,57 @@ class Presentation extends Specification {
         thirdNodeProposal.getValue() == valueLong
 
     }
+
+
+    def testAcceptorResponseFailure() {
+/*
+        Client   Proposer      Acceptor     Learner
+        |         |          |  |  |       |  |
+        X-------->|          |  |  |       |  |  Request
+        |         X--------->|->|->|       |  |  Prepare(1)
+        |         |          |  |  !       |  |  !! FAIL !!
+        |         |<---------X--X          |  |  Promise(1,{null,null})
+        |         X--------->|->|          |  |  Accept!(1,V)
+        |         |<---------X--X--------->|->|  Accepted(1,V)
+        |<---------------------------------X--X  Response
+        |         |          |  |          |  |*/
+
+        given:
+        HttpEntity<?> entity = new HttpEntity<>();
+        Random r = new Random();
+        Integer value = r.nextInt();
+        Long valueLong = value;
+        String key = "abck"+r.nextInt()
+        String params = "?key="+key+"&value=" + value
+        String url = second.getNodeUrl() + ApplicationEndpoints.CLIENT_PROPOSE_URL + params;
+        MultiValueMap<String, String> paramsMap = new LinkedMultiValueMap<String, String>();
+
+
+        when:
+        Proposal response = restTemplate.getForObject(url, Proposal.class);
+        //ubić akceptora 1
+        value = r.nextInt();
+        valueLong = value;
+        key = "abck"+r.nextInt()
+        params = "?key="+key+"&value=" + value
+        url = second.getNodeUrl() + ApplicationEndpoints.CLIENT_PROPOSE_URL + params;
+        response = restTemplate.getForObject(url, Proposal.class);
+
+
+
+        then:
+        //retrieve
+        String queryUrl = second.getNodeUrl() + ApplicationEndpoints.CLIENT_RETRIEVE_URL + params;
+        Integer responseValue = restTemplate.getForObject(queryUrl, Integer.class);
+        responseValue == valueLong
+
+        //check learners
+        //AcceptedProposal firstNodeProposal = restTemplate.getForObject(first.getNodeUrl() + ApplicationEndpoints.LERNER_URL + "/"+key, AcceptedProposal.class);
+        //firstNodeProposal.getValue() == valueLong
+        AcceptedProposal secondNodeProposal = restTemplate.getForObject(second.getNodeUrl() + ApplicationEndpoints.LERNER_URL + "/"+key, AcceptedProposal.class);
+        secondNodeProposal.getValue() == valueLong
+        AcceptedProposal thirdNodeProposal = restTemplate.getForObject(third.getNodeUrl() + ApplicationEndpoints.LERNER_URL + "/"+key, AcceptedProposal.class);
+        thirdNodeProposal.getValue() == valueLong
+
+    }
 }
