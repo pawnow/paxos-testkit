a1a9d7ef22eddbdd64c349bdcd74a94c2c97f674
Pawel Nowotarski <pawnow6@gmail.com>
2016-11-18 01:03:38 +0100

    Changed one url

--- src/main/java/pl/edu/agh/iosr/utils/NodesProvider.java
+++ src/main/java/pl/edu/agh/iosr/utils/NodesProvider.java
@@ -8,9 +8,9 @@ import java.util.List;
 public class NodesProvider {
 
     private static List<Node> nodes = Lists.newArrayList(
-            new Node(1L, "http://localhost:8081"),
-            new Node(2L, "http://localhost:8082"),
-            new Node(3L, "http://localhost:8083")
+            new Node(1L, "http://paxos4.eu-west-1.elasticbeanstalk.com:80"),
+            new Node(2L, "http://paxos2.eu-west-1.elasticbeanstalk.com:80"),
+            new Node(3L, "http://paxos3.eu-west-1.elasticbeanstalk.com:80")
     );
 
     public static List<Node> getListOfApplicationNodes(){
